f1a83357172c27be9b33c3334be0727c5bb12557
Alexander Koskovich <akoskovich@pm.me>
2022-04-12 05:00:05 -0700

    sm6125-common: Pick up USB configurations from QCOM
    Change-Id: I83882d8992eb367211871130c163f4ca5689fcd4

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -210,7 +210,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -233,7 +234,9 @@ PRODUCT_BOOT_JARS += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.basic
+    android.hardware.usb@1.3-service.basic \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh
 
 # Update engine
 PRODUCT_HOST_PACKAGES += \
