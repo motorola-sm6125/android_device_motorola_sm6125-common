16c5467eb7f6364946bc1cb5bd692592df3ea0e5
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-02 11:54:54 +0700

    sm6125-common: Import Wi-Fi configuration from stock and build its packages
    sofiap_ao-user 12 S0PRS32M.44-11-10-26 86991 release-keys
    
    * Straight build AIDL WiFi HAL because we're building 14 QPR2.
    
    Change-Id: Iff322d450c19ae1326b6c6e3c3d855cf8cf65ffd

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -127,5 +127,19 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Wi-Fi
+PRODUCT_PACKAGES += \
+    android.hardware.wifi-service \
+    hostapd \
+    libqsap_sdk \
+    libwifi-hal-qcom \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/sm6125-common/sm6125-common-vendor.mk)
