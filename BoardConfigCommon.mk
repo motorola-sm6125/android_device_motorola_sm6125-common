337007672fff8eeabfd2c824981c626edc06de3f
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-02 10:24:40 +0700

    sm6125-common: Add arch, bootloader and platform flags
    Change-Id: I68196f2a2a0bf06596c741741296ed3a8c47c223

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -5,5 +5,29 @@
 
 COMMON_PATH := device/motorola/sm6125-common
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a73
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a73
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := trinket
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := trinket
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
+
 # Include the proprietary files
 include vendor/motorola/sm6125-common/BoardConfigVendor.mk
