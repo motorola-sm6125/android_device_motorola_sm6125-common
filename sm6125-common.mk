8c5ac8ae86ff23508b3c2f976109e67c707b35ec
Christian Oder <myself5@carbonrom.org>
2024-07-02 11:16:36 +0700

    sm6125-common: Add basic permission file for HotwordEnrollment
    Change-Id: I8e78b229895a1471323e909ec2d3b5bf9644ffb9

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -23,6 +23,10 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
+# HotwordEnrollement app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-hotword.xml
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
