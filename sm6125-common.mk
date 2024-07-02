c2054c13129407ac20a56d26d54595a31a8dcf47
Anton Hansson <hansson@google.com>
2024-07-02 12:01:57 +0700

    sm6125-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -123,6 +123,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# Runtime Resource Overlay
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
