6b789ef19864715c59a2ec5503c21183bb16f685
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-06 00:40:05 +0700

    sm6125-common: Build Bluetooth stack
    Change-Id: I50ecdfd6d61cb29e43749a9f7951bd8c407d94b0
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -52,6 +52,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    audio.bluetooth.default \
+    android.hardware.bluetooth.audio-impl
+
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
