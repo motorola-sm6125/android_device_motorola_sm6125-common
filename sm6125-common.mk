d752b9dae2d91323c32590ee9d5b7637355dc822
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-06 01:24:11 +0700

    sm6125-common: Build telephony stack
    Change-Id: I43b7db05d5498dab04f3b8e580ecf34cdba0d6ce
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- sm6125-common.mk
+++ sm6125-common.mk
@@ -177,6 +177,25 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Telephony
+PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
