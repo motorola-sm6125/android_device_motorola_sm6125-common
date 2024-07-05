6bd90317c63d6c9a72c5acb68a8eb3e074c7dd7a
jmpfbmx <jmpf.bmx@gmail.com>
2024-07-06 00:36:29 +0700

    sm6125-common: Build ANT+ stack
    Change-Id: Ifba483de088180aa28144fe740921c33843cd9fe
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,6 +9,9 @@ COMMON_PATH := device/motorola/sm6125-common
 AB_OTA_UPDATER := true
 AB_OTA_PARTITIONS += boot dtbo system vendor vbmeta
 
+# ANT+
+BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
