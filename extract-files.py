6145ccb8c3ff07c2f9e17bd12360744656342b20
Han Sol Jin <hansol@hansol.ca>
2025-06-28 20:22:34 -0700

    sm6125-common: add initial blobs list
    Change-Id: Ie240f76936b8ccc43542fe4b643939a22bc308db

--- extract-files.py
+++ extract-files.py
@@ -4,12 +4,20 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_lib import (
+    lib_fixups,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
 namespace_imports = [
+    'hardware/motorola',
+    'hardware/qcom-caf/sm8150',
+    'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
 ]
 
 
@@ -17,6 +25,7 @@ module = ExtractUtilsModule(
     'sm6125-common',
     'motorola',
     namespace_imports=namespace_imports,
+    lib_fixups=lib_fixups,
 )
 
 
