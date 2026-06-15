2da283a0dba366ec6fe329f9672d37286ebe906d
Tuan Anh <tuan73176@gmail.com>
2026-06-15 02:35:06 +0000

    sm6125-common: Setup ELF checks
    Change-Id: I39d7c0b9db541e5f4131681aa783994d07c64de7

--- extract-files.py
+++ extract-files.py
@@ -4,8 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
 from extract_utils.fixups_lib import (
     lib_fixups,
+    lib_fixups_user_type,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -20,11 +25,31 @@ namespace_imports = [
     'vendor/qcom/opensource/display',
 ]
 
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/lib-imsvideocodec.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
+    ('vendor/lib64/libwvhidl.so', 'vendor/lib64/mediadrm/libwvdrmengine.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+}
 
 module = ExtractUtilsModule(
     'sm6125-common',
     'motorola',
     namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
 )
 
