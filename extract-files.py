1227f98e47f20dfa9ab503681223091ea7436d6e
Tuan Anh <tuan73176@gmail.com>
2026-06-21 09:05:18 +0000

    sm6125-common: Initial vendor blobs
    - Heavily based on moto-common/android_device_motorola_amogus@de72dd4f7dbf63caa93e338bf24d962c18601b2c
    - Also add props and manifest while at it.
    
    Change-Id: I4f3ccb63af0615e473071f68622ded127afd4f81

--- extract-files.py
+++ extract-files.py
@@ -4,14 +4,64 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
 
+namespace_imports = [
+    'hardware/qcom-caf/sm8150',
+    'vendor/qcom/opensource/dataservices',
+    'vendor/qcom/opensource/display',
+]
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib/hw/camera.trinket.so': blob_fixup()
+        .add_needed('libcamera_shim.so')
+        .binary_regex_replace(b'_ZN7android17CameraHalWatchdog12AutoWatchdog13startWatchdogEv', b'_ZN7android17CameraHalWatchdog12AutoWatchdog13startWatchhogEv')
+        .binary_regex_replace(b'libthermalclient', b'libthermapclient'),
+    'vendor/lib/libmmcamera2_pproc_modules.so': blob_fixup()
+        .binary_regex_replace(b'ro.product.manufacturer', b'ro.broduct.manufacturer'),
+    'vendor/lib/libmot_gpu_mapper.so': blob_fixup()
+        .add_needed('libgbp_shim.so'),
+    'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
+        .regex_replace('system', 'system_ext'),
+    ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
+        .add_needed('libcrypto_shim.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
+    'vendor/etc/wfdconfig.xml': blob_fixup()
+        .regex_replace('<AudioStreamInSuspend>0</AudioStreamInSuspend>', '<AudioStreamInSuspend>1</AudioStreamInSuspend>')
+        .regex_replace('<HID>0</HID>', '<HID>1</HID>'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'sm6125-common',
     'motorola',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
